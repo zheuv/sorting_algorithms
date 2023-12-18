9edc1f7a7c0b3ffd638fd1568309d004a06c0c95
mouhcine wildane <muhssenwildane@gmail.com>
2023-12-18 18:08:26 +0100

    0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+
+
+
+struct information is_array_sorted(int *array, size_t size)
+{
+	int i;
+	int j;
+	information info;
+
+	for (i = 0; i < (size -1); i++)
+	{
+		j = i + 1;
+		if array[i] > array[j]
+		{
+			info.myBoolean = false;
+			info.myInteger = i;
+			return info;
+		}
+	}
+	info.myBoolean = true;
+	info.myInteger = 0;
+	return info;
+}
+
+void bubble_sort(int *array, size_t size)
+{
+	int i;
+	int j;
+	struct information info;
+	
+	info = is_array_sorted(array, size);
+
+	while  (info.myBoolean == false)
+	{
+		for (i = info.myInteger; i < (size - 1); i++)
+		{
+			j = i + 1;
+
+			if (array[i] > array[j])
+			{
+				array[i] = array[i] + array[j];
+				array[j] = array[i] - array[j];
+				array[i] = array[i] - array[j];
+				print_array(array, size);
+			}
+			else
+			{
+				break;
+			}
+		}
+		info = is_array_sorted(array, size);
+	}
+}
+
+
+
+		
---
+++ sort.h
@@ -0,0 +1,62 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct information - Holds information about array sorting
+ * @myBoolean: Indicates if the array is sorted
+ * @myInteger: Position where sorting needs to resume
+ */
+struct information
+{
+    int myBoolean;
+    size_t myInteger;
+};
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size);
+
+/**
+ * print_list - Prints a list of integers
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list);
+
+/**
+ * is_array_sorted - Checks if an array is sorted
+ * @array: The array to check
+ * @size: Number of elements in @array
+ * Return: A struct containing information about array sorting
+ */
+struct information is_array_sorted(int *array, size_t size);
+
+/**
+ * bubble_sort - Sorts an array using the bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
+
